4b51fc2936d95c2c6778a69e2ac08e1781ed8a39
baodoanusth <baodoan1131996@gmail.com>
2016-12-03 21:50:52 +0700

    profile pic and name

--- FacebookClient/app/src/main/java/com/example/linhtynny/facebookclient/LoginActivity.java
+++ FacebookClient/app/src/main/java/com/example/linhtynny/facebookclient/LoginActivity.java
@@ -1,23 +1,59 @@
 package com.example.linhtynny.facebookclient;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-/**
- * Created by linhtynny on 20/11/2016.
- */
 
 public class LoginActivity extends AppCompatActivity {
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private TextView btnLogin;
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.android.facebookloginsample",  // replace with your unique package name
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
         /*setContentView(R.layout.activity_github);*/
         Log.i("log1", "This is log from onCreate()");
         setContentView(R.layout.fragment_login);
+
         Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
